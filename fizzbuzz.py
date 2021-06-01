be0b11dd83c0481b60d269a8a67ed77de7708e49
Miklos Bowling <miklos.bowling@gmail.com>
2021-05-31 21:26:52 -0700

    Implemented full length fizzbuzz generation

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,15 @@
 def fizzbuzz():
-    return [1, 2, 'Fizz', 4, 'Buzz']
+    arr = list(range(1, 101))
+    for i in range(100):
+        if arr[i] % 3 == 0:
+            if arr[i] % 5 == 0:
+                arr[i] = 'FizzBuzz'
+            else:
+                arr[i] = 'Fizz'
+        elif arr[i] % 5 == 0:
+            arr[i] = 'Buzz'
+    return arr
+            
 
 
 if __name__ == '__main__':
