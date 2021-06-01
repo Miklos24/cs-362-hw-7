5eb4232d3926e5ff31e3c1962e1157f60d3a3244
Miklos Bowling <miklos.bowling@gmail.com>
2021-05-31 21:32:25 -0700

    Wrote second test for % 100 year test case

--- test_leapyear.py
+++ test_leapyear.py
@@ -4,6 +4,9 @@ import unittest
 class TestLeapYear(unittest.TestCase):
     def test_year4(self):
         self.assertTrue(isLeapYear(4))
+    
+    def test_year100(self):
+        self.assertFalse(isLeapYear(100))
 
 if __name__ == '__main__':
     unittest.main()
