8d8e2bb8c9dea640c5a6391663e66236694241b8
Miklos Bowling <miklos.bowling@gmail.com>
2021-05-31 21:19:36 -0700

    Wrote second test, testing "buzz" on 5th element

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -4,6 +4,9 @@ import unittest
 class TestFizzbuzz(unittest.TestCase):
     def test_fizz(self):
         self.assertEqual(fizzbuzz()[2], 'Fizz')
+    
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz()[4], 'Buzz')
 
 if __name__ == '__main__':
     unittest.main()
